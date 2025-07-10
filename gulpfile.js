7a595f6cb97adaba186365514936f976261c2c60
Chavira-cloud <markos.mgcg@gmail.com>
2025-07-10 16:06:34 -0700

    Avance de festival cpon SASS

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,11 @@ const sass = gulpSass(dartSass)
 
 export  function css(done){ // con export se hace detectable la funcion en el gulp
     src('src/scss/app.scss')
-    .pipe(sass ())// pipe() SIRVE PARA COMPILAR 
-    .pipe(dest('dist/css'))
+    .pipe( sass().on('error',sass.logError))// pipe() SIRVE PARA COMPILAR 
+    .pipe( dest('dist/css'))
     done() // Finaliza la tarea cuando llamamos el script (npm run nombre_del script)
 }
 
 export function dev(){
-    watch('src/scss/app.scss', css)
+    watch('src/scss/**/*.scss', css)
 }
