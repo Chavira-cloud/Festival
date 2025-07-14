2db540af37690f2b4112d7072b75971b106a2359
Chavira-cloud <markos.mgcg@gmail.com>
2025-07-14 16:54:16 -0700

    terminacion de festival css 14-05-25

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,26 @@
-import {src, dest, watch} from 'gulp'
+import {src, dest, watch, series} from 'gulp'
 import * as dartSass from 'sass'
 import gulpSass from 'gulp-sass'
 
 const sass = gulpSass(dartSass)
 
 
+export function js (done){
+    src('src/js/app.js')
+    .pipe(dest('dist/js'))
+    done()
+}
+
 export  function css(done){ // con export se hace detectable la funcion en el gulp
-    src('src/scss/app.scss')
-    .pipe( sass().on('error',sass.logError))// pipe() SIRVE PARA COMPILAR 
-    .pipe( dest('dist/css'))
+    src('src/scss/app.scss',{sourcemaps: true})
+    .pipe( sass().on('error', sass.logError))// pipe() SIRVE PARA COMPILAR 
+    .pipe( dest('dist/css', {sourcemaps: true}))
     done() // Finaliza la tarea cuando llamamos el script (npm run nombre_del script)
 }
 
 export function dev(){
     watch('src/scss/**/*.scss', css)
+    watch('src/js/**/*.js', js)
 }
+
+export default series(js, css, dev)
---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+    crearGaleria()
+})
+
+function crearGaleria(){
+    console.log('desde crear galaeria');
+}
