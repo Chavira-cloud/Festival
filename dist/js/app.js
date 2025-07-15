be58586f35861dd4c1281dc406c7b112d8115769
Chavira-cloud <markos.mgcg@gmail.com>
2025-07-15 16:54:13 -0700

    avance modal de galeria festival

--- dist/js/app.js
+++ dist/js/app.js
@@ -3,5 +3,49 @@ document.addEventListener('DOMContentLoaded', function(){
 })
 
 function crearGaleria(){
-    console.log('desde crear galaeria');
+    const galeria = document.querySelector('.galeria-imagenes')
+    const numImagenes = 16
+    for (let i= 1; i <= numImagenes; i++) {
+        const imagen = document.createElement('IMG')
+        imagen.src= `src/img/gallery/full/${i}.jpg`
+        imagen.alt = 'Imagen Galeria'
+
+        //Even handler
+        imagen.onclick = function(){
+            mostarimagen(i)
+        }
+
+        galeria.appendChild(imagen)
+    }
+}
+
+function mostarimagen(i){
+    const imagen = document.createElement('IMG')
+    imagen.src= `src/img/gallery/full/${i}.jpg`
+    imagen.alt = 'Imagen Galeria'
+
+
+    //Genrar modal
+    const modal = document.createElement('DIV')
+    modal.classList.add('modal')
+    modal.onclick = cerrarModal
+
+    modal.appendChild(imagen)
+
+    //agregar al HTML
+    const body = document.querySelector('body')
+    body.classList.add('overflow-hidden');
+    body.appendChild(modal)
+}
+
+function cerrarModal(){
+    const modal = document.querySelector('.modal')
+    modal.classList.add('fade-out')
+
+    setTimeout(()=>{
+        modal?.remove()
+
+        const body = document.querySelector('body')
+        body.classList.remove('overflow-hidden');
+    },500);
 }
